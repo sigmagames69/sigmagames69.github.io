1abebce9ad456d7da9880c331ddb5a937734cfe8
Joe <167563398+Joercat@users.noreply.github.com>
2024-12-04 09:34:56 -0500

    Update data2.js

--- data2.js
+++ data2.js
@@ -10,6 +10,9 @@
       { name: "Blocky Browser", url: "/services/browser", query: "browser" },
       { name: "Blocky Browser", url: "/services/browser", query: "blocky browser" },
 
+      { name: "Calculator", url: "/services/calculator", query: "math" },
+      { name: "Calculator", url: "/services/calculator", query: "calculator" },
+
       { name: "Deal or no Deal", url: "/deal-or-no-deal", query: "deal" },
       { name: "Deal or no Deal", url: "/deal-or-no-deal", query: "deal or no deal" },
 
